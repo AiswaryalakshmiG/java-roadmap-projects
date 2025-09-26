cc66f8b9d7f8ef71d05b3586f57bf74c45a6d45e
AiswaryalakshmiG <aiswaryalakshmi1502@gmail.com>
2025-09-26 23:13:14 +0530

    StoreNames

---
+++ src/stringarray/StoreNames.java
@@ -0,0 +1,12 @@
+package stringarray;
+
+public class StoreNames {
+    public static void main(String[] args) {
+        String[] names = {"Aishu", "Sri", "Ramya", "Priya"};
+
+        System.out.println("Names in array:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
